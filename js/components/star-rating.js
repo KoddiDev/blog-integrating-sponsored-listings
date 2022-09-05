ba76916d4a5c694d10c5f2383c2d19ad863480ee
Matt Sollars <matt.sollars@koddi.com>
2022-09-05 17:54:22 -0400

    Fix StarRating to render in connectedCallback

--- js/components/star-rating.js
+++ js/components/star-rating.js
@@ -1,18 +1,32 @@
 class StarRating extends HTMLElement {
     constructor() {
         super();
+    }
+
+    get value() {
+        return this.getAttribute('value');
+    }
+
+    set value(value) {
+        this.setAttribute('value', value);
+    }
+
+    connectedCallback() {
+        if (!this.isConnected) return;
         this.render();
     }
 
     render() {
-        const value = this.getAttribute('value');
+        if (this._isRendered) return;
 
         this.innerHTML = `
             <div class="rating">
-                <progress value="${value}" max="5">3.3 of 5.0</progress>
+                <progress value="${this.value}" max="5">3.3 of 5.0</progress>
                 <svg><use href="#fiveStars" /></svg>
             </div>
         `;
+
+        this._isRendered = true;
     }
 }
 
