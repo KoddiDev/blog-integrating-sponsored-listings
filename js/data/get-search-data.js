09f33177596e2ab076077ba6ed7599709850932c
Matt Sollars <matt.sollars@koddi.com>
2022-09-05 18:48:00 -0400

    Add option to exclude winning ads from search

--- js/data/get-search-data.js
+++ js/data/get-search-data.js
@@ -1,14 +1,17 @@
 import delay from '../lib/delay.js';
-import { acmeSlingshotProducts, acmeSlingshotWinningAds } from "../data/acme-slingshot-data";
+import { acmeSlingshotProducts, acmeSlingshotWinningAds } from "./acme-slingshot-store";
 
 
 // NOTE: Currently, we're ignoring search parameters as this is a simulation.
 
-export async function getConsolidatedSearchData(searchParameters, delayInMilliseconds = 0) {
+export async function getConsolidatedSearchData(searchParameters, delayInMilliseconds = 0, excludeWinningAds = false) {
     const searchResults = await getSearchProducts(searchParameters);
     const winningAds = await getSearchWinningAds(searchParameters, delayInMilliseconds);
 
-    return { searchResults, winningAds };
+    return {
+        searchResults, 
+        winningAds: excludeWinningAds ? [] : winningAds
+    };
 }
 
 export async function getSearchProducts(searchParameters, delayInMilliseconds = 0) {
