69368765f18f3ab61304b4fe0f5fbc441bc088aa
Matt Sollars <matt.sollars@koddi.com>
2022-09-15 22:37:32 -0400

    Update store with odd result count and longer desc

--- js/data/acme-slingshot-store.js
+++ js/data/acme-slingshot-store.js
@@ -3,9 +3,10 @@ const plasticSlingshot = { title: 'Plastic Slingshot', price: 34.99, rating: 3.3
 const ironSlingshot = { title: 'Iron Slingshot', price: 74.99, rating: 2.8, summary: 'Less breaking! Sling Stuff™' };
 const steelSlingshot = { title: 'Steel Slingshot', price: 99.99, rating: 4.2, summary: 'Reinforced for improved launching. Sling Stuff™' };
 const goldenSling = { title: 'Golden Sling', price: 299.99, rating: 3.5, summary: 'Hang our golden sling in your library to show off to guests!' };
-const coyoteSizedSlingshot = { title: 'Coyote-sized Slingshot', price: 319.99, rating: 2.1, summary: 'Fling yourself over large distances now! Intended for Coyotes only.' };
+const coyoteSizedSlingshot = { title: 'Coyote-sized Slingshot', price: 319.99, rating: 2.1, summary: 'Shoot yourself over large distances now! Intercept small game. Get places fast. Intended for Coyotes only.' };
 const titaniumSlingshot = { title: 'Titanium Slingshot', price: 349.99, rating: 4.6, summary: 'Really dense for extreme use. Sling Stuff™' };
 const adamantiumSlingshot = { title: 'Adamantium Slingshot', price: 449.99, rating: 4.5, summary: 'Sling Stuff™ It\'s unbreakable!' };
+const flingSling = { title: 'Fling Sling', price: 37.49, rating: 1.4, summary: 'Fling small objects for immense fun.' };
 
 const oneYearSlignshotWarranty = { title: 'Acme 1-Year Slingshot Warranty', price: 25.00, rating: 3.7, summary: 'Protect your new slingshot for 1 year.' };
 const twoYearSlignshotWarranty = { title: 'Acme 2-Year Slingshot Warranty', price: 35.00, rating: 4.2, summary: 'Protect your new slingshot for 2 years.' };
@@ -23,6 +24,7 @@ export const acmeSlingshotProducts = [
     adamantiumSlingshot,
     twoYearSlignshotWarranty,
     threeYearSlignshotWarranty,
+    flingSling,
 ];
 
 export const acmeSlingshotWinningAds = [
